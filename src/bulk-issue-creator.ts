64a6c96f906e77136c2c7b6432f2f3de5e103a09
Ben Balter <ben@balter.com>
2024-02-03 16:23:16 -0500

    initial ts

---
+++ src/bulk-issue-creator.ts
@@ -0,0 +1,157 @@
+import * as fs from 'fs';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as changeCase from "change-case";
+import { parse } from 'csv-parse/sync';
+import { Issue } from './issue';
+import * as yaml from 'js-yaml';
+import { RequestError } from '@octokit/request-error';
+import { GitHub } from "@actions/github/lib/utils"
+
+export class BulkIssueCreator {
+  optionKeys = ["template_path", "csv_path", "write", "comment", "github_token"]
+  boolOptions = ["write","comment"]
+  defaultTemplatePath = './config/template.md.mustache'
+  defaultCsvPath = './config/data.csv'
+  octokit: InstanceType<typeof GitHub>;
+  options = {
+    templatePath: this.defaultTemplatePath,
+    csvPath: this.defaultCsvPath,
+    write: false,
+    comment: false,
+    githubToken: null
+  }
+
+  constructor(passedOptions: {}) {
+    for (const key of this.optionKeys) {
+      const value = passedOptions[key] || core.getInput(key.toUpperCase()) || null;
+      const camelCaseKey = changeCase.camelCase(key);
+      this.options[camelCaseKey] = this.boolOptions.includes(key) ? this.truthy(value) : value;
+    }
+
+    const token = core.getInput('token', { required: true });
+    this.octokit = github.getOctokit(token);
+  }
+
+  get templatePath() {
+    return this.options.templatePath || this.defaultTemplatePath
+  }
+
+  get csvPath() {
+    return this.options.csvPath || this.defaultCsvPath
+  }
+
+  get readonly() {
+    return this.options.write !== true
+  }
+
+  get write() {
+    return !this.readonly
+  }
+
+  get comment() {
+    return this.options.comment === true
+  }
+
+  get template() {
+    return fs.readFileSync(this.templatePath, 'utf8');
+  }
+
+  get issues() {
+    let issues = []
+    for (const row of this.data) {
+      const issue = new Issue(row, this.template)
+      issues.push(issue)
+    }
+    return issues;
+  }
+
+  run() {
+    core.info(`Running with options: ${yaml.dump(this.options)}`)
+    
+    this.ensurePathExists(this.templatePath)
+    this.ensurePathExists(this.csvPath)
+
+    if (this.readonly) {
+      this.previewOutput()
+      return;
+    }
+
+    if (this.comment) {
+      this.createComments();
+    } else {
+      this.createIssues();
+    }
+  }
+
+  private createIssues() {
+    let response;
+    for (const issue of this.issues) {
+      response = this.octokit.rest.issues.create({
+        owner: issue.nwo[0],
+        repo: issue.nwo[1],
+        title: issue.title,
+        body: issue.body,
+        labels: issue.labels,
+        assignees: issue.assignees
+      });
+    }
+    core.info(`Created issue ${response.data.html_url}`);
+  }
+
+  private createComments() {
+    for (const issue of this.issues) {
+      const response = this.octokit.rest.issues.createComment({
+        owner: issue.nwo[0],
+        repo: issue.nwo[1],
+        issue_number: issue.number,
+        body: issue.body
+      });
+      core.info(`Created comment ${response.data.html_url}`);
+    }
+  }
+
+  private get data() {
+    const csv = fs.readFileSync(this.csvPath, 'utf8');
+    return parse(csv, { columns: true });
+  }
+
+  private ensurePathExists(path: string) { 
+    if (!fs.existsSync(path)) {
+      throw new Error(`Path does not exist: ${path}`);
+    }
+  }
+  
+  private repoExists(nwo: string) {
+    const [owner, repo] = nwo.split('/');
+    try { 
+      this.octokit.rest.repos.get({
+        owner,
+        repo,
+      });
+    } catch (error) {
+      if (error instanceof RequestError && error.status === 404) {
+        core.warning(`Repository ${nwo} does not exist. Skipping...`)
+        return false;
+      } else {
+        throw error;
+      }
+    }
+
+    return true;
+  }
+
+  private truthy(value: any) {
+    return value === true || value === "true"
+  }
+
+  private previewOutput() {
+    core.info("Running in READ ONLY mode. Pass `WRITE=true` environmental variable to write.")
+    core.info(`the following ${this.comment ? "comments" : "issues"} would have been created:`)
+
+    for (const issue of this.issues) {
+      this.repoExists(issue.repository);
+      core.info(yaml.dump(issue.data));
+    }
+  }
+}
---
+++ src/issue.ts
@@ -0,0 +1,51 @@
+import * as mustache from 'mustache';
+import * as changeCase from "change-case";
+
+export interface IssueData {
+  title: string;
+  body: string;
+  labels: string;
+  assignees: string;
+  repository: string;
+  issue_number: string;
+  [key: string]: string | string
+}
+
+export class Issue {
+  data: IssueData;
+  template: string;
+
+  constructor(data: IssueData, template: string) {
+    this.data = data;
+    this.template = template;
+  }
+
+  get title() {
+    return mustache.render(this.data.title, this.data);
+  }
+  
+  get body() {
+    return mustache.render(this.template, this.data);
+  }
+
+  get labels() {
+    return this.data.labels.split(',').map(label => label.trim());
+  }
+
+  get assignees() {
+    return this.data.assignees.split(',').map(assignee => assignee.trim());
+  }
+
+  get repository() {
+    return this.data.repository;
+  }
+
+  get number() {
+    return this.data.issue_number;
+  }
+
+  get nwo() {
+    return this.data.repository.split('/');
+  }
+}
+
